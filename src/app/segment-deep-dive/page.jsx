cd0b40c051e48828d16a11c163a1e08dcefd8735
cody-SAT <codyjameskim@gmail.com>
2025-11-14 16:07:38 -0500

    Create page.jsx

---
+++ src/app/segment-deep-dive/page.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+
+export default function SegmentDeepDivePage() {
+  return (
+    <div className="space-y-6">
+      <Card>
+        <CardHeader>
+          <CardTitle>Segment Deep Dive</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <p className="text-gray-500">
+            This page is coming soon. It will contain the deep-dive sub-tabs for:
+          </p>
+          <ul className="list-disc pl-6 mt-4 text-gray-700">
+            <li>Google Search</li>
+            <li>YouTube</li>
+            <li>Google Cloud</li>
+            <li>Other Bets</li>
+          </ul>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
