8bf37b4f5e6a0096ddcacf13068e7e2ab897b219
cody-SAT <codyjameskim@gmail.com>
2025-11-14 23:22:47 -0500

    Update page.jsx

--- src/app/segment-deep-dive/page.jsx
+++ src/app/segment-deep-dive/page.jsx
@@ -1,23 +1,14 @@
-"use client" // All pages with components need this
+"use client"
 import React from 'react';
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
+import { Card, CardContent, CardHeader, CardTitle } from '../../components/ui/card'; // FIX: Relative path
 
 export default function SegmentDeepDivePage() {
   return (
     <div className="space-y-6 animate-fadeIn">
       <Card>
-        <CardHeader>
-          <CardTitle>Segment Deep Dive</CardTitle>
-        </CardHeader>
+        <CardHeader><CardTitle>Segment Deep Dive</CardTitle></CardHeader>
         <CardContent>
-          <p className="text-gray-500">
-            This page is coming soon. It will contain the deep-dive sub-tabs for:
-          </p>
+          <p className="text-gray-500">This page is coming soon. It will contain the deep-dive sub-tabs for:</p>
           <ul className="list-disc pl-6 mt-4 text-gray-700">
             <li>Google Search</li>
             <li>YouTube</li>
